dc2f4e7a0b751d735c6d6c75d094e698c19f3037
Stefan <sjslagter@hotmail.com>
2025-09-07 20:16:11 +0200

    added rules

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -84,39 +84,39 @@ window.APIClient = {
     async analyzeRequirements(request) {
         return this.post('/requirements/analyze', request);
     },
-    
+
     async getRequirements(analysisId) {
         return this.get(`/requirements/${analysisId}`);
     },
-    
+
     async canCreateProjectPlan(analysisId) {
         return this.get(`/projectplanning/can-create/${analysisId}`);
     },
-    
+
     async createProjectPlan(request) {
         return this.post('/projectplanning/create', request);
     },
-    
+
     async getProjectPlan(planningId) {
         return this.get(`/projectplanning/${planningId}`);
     },
-    
+
     async canGenerateStories(planningId) {
         return this.get(`/stories/can-generate/${planningId}`);
     },
-    
+
     async generateStories(request) {
         return this.post('/stories/generate', request);
     },
-    
+
     async getStories(storyGenerationId) {
         return this.get(`/stories/generations/${storyGenerationId}/results`);
     },
-    
+
     async canGenerateCode(storyGenId) {
         return this.get(`/code/can-generate/${storyGenId}`);
     },
-    
+
     async generateCode(request) {
         return this.post('/code/generate', request);
     },
@@ -165,10 +165,10 @@ window.APIClient = {
     },
 
     // Helper method to retrieve approved stories
-    async getApprovedStories(storyGenerationId) {
-        // Note: This endpoint may need to be created or use existing story endpoints
-        return await this.get(`/stories/generations/${storyGenerationId}/approved`);
-    },
+    // async getApprovedStories(storyGenerationId) {
+    //     // Note: This endpoint may need to be created or use existing story endpoints
+    //     return await this.get(`/stories/generations/${storyGenerationId}/approved`);
+    // },
 
     // Story management methods
     async approveStory(storyId) {
